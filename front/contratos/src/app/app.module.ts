628425d0b0232efb7e3534eb74cca2bbc8c91cc9
Elena <elena.ormaza.front.dp@gmail.com>
2019-11-12 13:44:02 +0100

    estilos generales

--- front/contratos/src/app/app.module.ts
+++ front/contratos/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
