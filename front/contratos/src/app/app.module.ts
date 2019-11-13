b43570af7dc3196dd71cd90ead9fbb3f93ca2264
Elena <elena.ormaza.front.dp@gmail.com>
2019-11-13 14:31:51 +0100

    ok

--- front/contratos/src/app/app.module.ts
+++ front/contratos/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { Login1Component } from './components/login1/login1.component';
+import { Registro2Component } from './components/registro2/registro2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    Login1Component
+    Login1Component,
+    Registro2Component
   ],
   imports: [
     BrowserModule,
