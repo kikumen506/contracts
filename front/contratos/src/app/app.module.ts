95ae1ed1f4bf47cec26d2804a48514a42ed89122
Elena <elena.ormaza.front.dp@gmail.com>
2019-11-18 11:11:46 +0100

    finalizado3

--- front/contratos/src/app/app.module.ts
+++ front/contratos/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { Login1Component } from './components/login1/login1.component';
 import { Registro2Component } from './components/registro2/registro2.component';
+import { Fin3Component } from './components/fin3/fin3.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { Registro2Component } from './components/registro2/registro2.component';
     NavbarComponent,
     FooterComponent,
     Login1Component,
-    Registro2Component
+    Registro2Component,
+    Fin3Component
   ],
   imports: [
     BrowserModule,
---
+++ front/contratos/src/app/components/fin3/fin3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fin3',
+  templateUrl: './fin3.component.html',
+  styleUrls: ['./fin3.component.css']
+})
+export class Fin3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
